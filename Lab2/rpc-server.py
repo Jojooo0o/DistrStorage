50c056bc7eeb2fa439956ed8e7e55e8945c49261
Jojooo0o <jonasdorsch@web.de>
2021-09-14 16:41:36 +0200

    Lab 2 task 6 done

--- Lab2/rpc-server.py
+++ Lab2/rpc-server.py
@@ -7,6 +7,9 @@ from tinyrpc.dispatch import RPCDispatcher
 from tinyrpc.protocols.jsonrpc import JSONRPCProtocol
 from tinyrpc.transports.wsgi import WsgiServerTransport
 
+import base64
+import random, string
+
 dispatcher = RPCDispatcher()
 transport = WsgiServerTransport(queue_class=gevent.queue.Queue)
 
@@ -25,9 +28,22 @@ def reverse_string(s):
     return s[::-1]
 
 @dispatcher.public
-def store_file(name, cont):
-    # Fill out shit later!
-    pass
+def store_file(filename, data):
+    binary_data = base64.b64decode(data)
+    base64_string = base64.b64encode(binary_data)
+    if not filename:
+        filename_len = 8
+        filename = ''.join([random.SystemRandom().choice(string.ascii_letters + string.digits) for n in range(filename_len)])
+        filename += '.bin'
+    try:
+        file = open('./'+filename, 'wb')
+        file.close()
+        with open('./'+filename, 'r+b') as f:
+            f.write(base64_string)
+    except EnvironmentError as e:
+        print(f'Error writing file: {e}')
+        return None
+    return filename
 
 
 # in the main greenlet, run our rpc_server
